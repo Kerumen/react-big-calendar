fc3c77e56d4ea3da78d6ac719e15e1dc85feeb1e
Mathieu Le Tyrant <mathieu.letyrant@gmail.com>
2018-12-26 18:08:53 +0100

    Fix isSpecialEvent

--- src/utils/eventLevels.js
+++ src/utils/eventLevels.js
@@ -74,33 +74,28 @@ export function segsOverlap(seg, otherSegs) {
   )
 }
 
-export function sortEvents(evtA, evtB, accessors) {
+export function sortEvents(
+  evtA,
+  evtB,
+  { start, end, sortBy, allDay, isSpecialEvent = () => true }
+) {
   let startSort =
-    +dates.startOf(accessors.start(evtA), 'day') -
-    +dates.startOf(accessors.start(evtB), 'day')
+    +dates.startOf(start(evtA), 'day') - +dates.startOf(start(evtB), 'day')
 
-  let durA = dates.diff(
-    accessors.start(evtA),
-    dates.ceil(accessors.end(evtA), 'day'),
-    'day'
-  )
+  let durA = dates.diff(start(evtA), dates.ceil(end(evtA), 'day'), 'day')
 
-  let durB = dates.diff(
-    accessors.start(evtB),
-    dates.ceil(accessors.end(evtB), 'day'),
-    'day'
-  )
+  let durB = dates.diff(start(evtB), dates.ceil(end(evtB), 'day'), 'day')
 
   return (
-    accessors.isSpecialEvent(evtA) === accessors.isSpecialEvent(evtB)
+    isSpecialEvent(evtA) === isSpecialEvent(evtB)
       ? 0
-      : accessors.isSpecialEvent(evtA)
+      : isSpecialEvent(evtA)
       ? -1
       : 1,
     startSort || // sort by start Day first
     Math.max(durB, 1) - Math.max(durA, 1) || // events spanning multiple days go first
-    !!accessors.allDay(evtB) - !!accessors.allDay(evtA) || // then allDay single day events
-    +accessors.start(evtA) - +accessors.start(evtB) || // then sort by start time
-      accessors.sortBy(evtA, evtB)
+    !!allDay(evtB) - !!allDay(evtA) || // then allDay single day events
+    +start(evtA) - +start(evtB) || // then sort by start time
+      sortBy(evtA, evtB)
   )
 }
